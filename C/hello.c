60b28155853eeacb3a1f4815af4adf6f68cf38e1
Jassem <jimmybouakaz30@gmail.com>
2026-06-03 18:31:59 +0100

    min & max array

--- C/hello.c
+++ C/hello.c
@@ -1,5 +1,26 @@
 #include<stdio.h>
 int main(void){
-printf("hello world ");
+int n , i , j , max , min ; 
+scanf("%d",&n);
+int arr[n];
+for(i=0 ; i<n ; i++ ){
+printf("the element number %d :",i);
+scanf("%d",&arr[i]);
+}
+
+max = arr[0];
+
+for(i=1;i<n;i++){
+                   if ( arr[i]>max){
+      max = arr[i];}
+}
+printf("\nmax = %d",max);
+min = arr[0];
+for(i=1;i<n;i++){
+    if(arr[i]<min){
+        min = arr[i];
+    }
+}
+printf("\nmin = %d ",min);
     return 0;
 }
